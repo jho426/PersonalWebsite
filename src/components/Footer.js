1d96691db1b08b9fae9cfcc14adf84d4e0c47c41
Jacqueline Ho <jacquelineho426@gmail.com>
2023-09-18 00:00:04 -0400

    fixing spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,12 +13,13 @@ const Footer = () => {
 
     return (
     <div id = "Footer" className = "w-full h-1/4 md:h-1/3 mb-0 mt-auto">
-        <WaveFunction/>
-        <div className = "w-full h-full bg-[#131313] mt-[-20px] pl-2">
-            <img draggable={false} className = "w-full md:w-5/6 px-4 mx-auto md:px-10" alt = "" src={FooterText}></img>
-            <div className = "text-[#A09E9E] w-full px-4 flex flex-row mt-5 mx-auto">
-                <p className = "text-[#A09E9E] tracking-tighter md:text-lg lg:text-lg my-auto" >© Jacqueline Ho – 2023</p>
-                <div className = "flex flex-row mr-0 ml-auto gap-2 text-md md:gap-5 lg:gap-5">
+        <div className = "w-full h-[100px] ">
+            <WaveFunction/>
+        </div>
+        <div className = "w-full bg-[#131313] h-[200px]">
+            <img draggable={false} className = "w-full md:w-5/6 px-4 py-5 mx-auto md:px-10" alt = "" src={FooterText}></img>
+            <div className = "text-[#A09E9E] bg-[#131313] w-full p-4 flex flex-row mx-auto">
+                <div className = "flex flex-row mr-0 ml-auto gap-2 text-xs md:text-md  lg:text-lg md:gap-5 lg:gap-5">
                     {data.map((link) => (
                         <a href = {link.link} target="_blank"><p className = "tracking-tighter px-5 py-1 border border-[#A09E9E] border-[1px] rounded-full hover:text-[#939e4f] hover:border-[#939e4f]">{link.name}</p></a>
                     ))}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,11 +27,12 @@ const Home = () => {
   });
 
   return (
-    <div className="flex flex-col w-full h-[100vh] items-center overflow-x-hidden">
+    <>
+    <div className="flex flex-col w-full h-[70vh] md:h-screen lg:h-screen items-center overflow-x-hidden">
       <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       {(!menuOpen || windowWidth > 768) && (
         <>
-          <div className="flex flex-col w-full mt-40 md:mt-10 items-center justify-center gap-10">
+          <div className="flex flex-col w-full my-auto items-center justify-center gap-10">
             <img
               className="w-5/6 lg:w-1/2"
               src={BannerImg}
@@ -47,8 +48,9 @@ const Home = () => {
           </div>
         </>
       )}
-      <Footer />
     </div>
+    <Footer />
+    </>
   );
 };
 
