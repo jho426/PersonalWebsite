90ad691a09900d350fd37ad4ac7eb73427681a6f
Jacqueline Ho <jacquelineho426@gmail.com>
2023-09-17 23:45:22 -0400

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,11 +14,11 @@ const Footer = () => {
     return (
     <div id = "Footer" className = "w-full h-1/4 md:h-1/3 mb-0 mt-auto">
         <WaveFunction/>
-        <div className = "w-full  h-full bg-[#131313] mt-[-20px] p-[20px]">
-            <img draggable={false} className = "w-full md:w-5/6 mx-auto md:px-10" alt = "" src={FooterText}></img>
-            <div className = "text-[#A09E9E] flex flex-row mt-10">
-                <p className = "text-[#A09E9E] tracking-tighter text-md my-auto" >© Jacqueline Ho – 2023</p>
-                <div className = "flex flex-row mr-0 ml-auto gap-2 md:gap-5 lg:gap-5">
+        <div className = "w-full h-full bg-[#131313] mt-[-20px] pl-2">
+            <img draggable={false} className = "w-full md:w-5/6 px-4 mx-auto md:px-10" alt = "" src={FooterText}></img>
+            <div className = "text-[#A09E9E] w-full px-4 flex flex-row mt-5 mx-auto">
+                <p className = "text-[#A09E9E] tracking-tighter md:text-lg lg:text-lg my-auto" >© Jacqueline Ho – 2023</p>
+                <div className = "flex flex-row mr-0 ml-auto gap-2 text-md md:gap-5 lg:gap-5">
                     {data.map((link) => (
                         <a href = {link.link} target="_blank"><p className = "tracking-tighter px-5 py-1 border border-[#A09E9E] border-[1px] rounded-full hover:text-[#939e4f] hover:border-[#939e4f]">{link.name}</p></a>
                     ))}
